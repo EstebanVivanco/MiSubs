496fc2b4ee517833bffe881d8eb98995f550817f
Esteban Vivanco <esteban.vivanco.progra@gmail.com>
2021-11-13 19:49:26 -0300

    interfaces

---
+++ app/src/main/java/com/example/misubs/Mantenedor_act.java
@@ -0,0 +1,14 @@
+package com.example.misubs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mantenedor_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mantenedor);
+    }
+}
